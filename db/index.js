b26ceb7c66d6dc8f50941aa569556a5143c564f7
M.T. Oyelowo <oyelowomayowa@gmail.com>
2024-03-07 09:16:46 +0100

    fs-blog backend V1

---
+++ db/index.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/fs-blog")
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log("db connection failed: ", err.message || err));
